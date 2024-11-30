f1b2a155e24c59d68108e34ab5f7e40c1cb745af
Anish <xevenbiswas@gmail.com>
2024-11-30 15:05:18 +0530

    new updatd packages

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,8 @@
 import { createOpenAI } from "@ai-sdk/openai";
-import { StreamingTextResponse, streamText } from "ai";
+import { streamText } from "ai";
 
 export const dynamic = "force-dynamic";
+export const maxDuration = 30;
 
 const groq = createOpenAI({
   apiKey: process.env.GROQ_API_KEY ?? "",
@@ -12,12 +13,13 @@ export async function POST(req: Request) {
   try {
     const { messages } = await req.json();
 
-    const result = await streamText({
-      model: groq.chat("llama3-70b-8192"), // llama3-8b-8192 , llama3-70b-8192, llama2-70b-4096,  mixtral-8x7b-32768, gemma-7b-it
+    const result = streamText({
+      model: groq.chat("llama3-70b-8192"), // llama3-8b-8192 , llama3-70b-8192, llama2-70b-4096,  mixtral-8x7b-32768, gemma-7b-it,
+      system: 'You are a helpful assistant.',
       messages,
     });
 
-    return new StreamingTextResponse(result.toAIStream());
+    return result.toDataStreamResponse();
   } catch (error) {
     console.log(error);
     throw error;
